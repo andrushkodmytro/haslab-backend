143634c14e89717caf6ad16fcd3d06b9bb5e0e26
dmytro <andrushkodima@gmail.com>
2021-02-15 10:59:00 +0200

    Add

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(express.json({ extended: false }))
 
 var corsOptions = {
   origin: 'http://localhost:3000',
+  credentials:true, 
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
